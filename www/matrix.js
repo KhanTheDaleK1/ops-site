be9af366f110e05ef6a5ac83639058113c589957
Evan Beechem <evan.t.beechem@gmail.com>
2026-01-08 13:09:52 -0600

    feat: Update matrix canvas ID and modify styles for debugging

--- www/matrix.js
+++ www/matrix.js
@@ -1,4 +1,4 @@
-const canvas = document.getElementById('matrix-canvas');
+const canvas = document.getElementById('matrix');
 const ctx = canvas.getContext('2d');
 
 canvas.width = window.innerWidth;
